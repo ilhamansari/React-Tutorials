5be3381fe17135bd7cd853619ef0128aac2a4f63
Md Ilham Ansari <ansariilham5@gmail.com>
2024-01-04 11:48:29 +0530

    Day 2 commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -14,7 +14,7 @@ function Cards({id, name, info, image, price,  removeTour}) {
             <img src={image} className="image"></img>
             <div className="tour-info">
                 <div className="tour-details">
-                <h4 className="tour-price">{price}</h4>
+                <h4 className="tour-price">${price}</h4>
                 <h4 className="tour-name">{name}</h4>
             </div>
 
--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -4,12 +4,12 @@ function Tours({tours, removeTour}){
     return(
         <div className="container">
             <div>
-                <h2 className="title">Plan with Ilham </h2>
+                <h2 className="title">Plan with ILHAM </h2>
             </div>
             <div className="cards">
                 {
                     tours.map((tour)=>{
-                        return <Cards {...tour} removeTour = {removeTour}/>
+                        return <Cards key={tour.id} {...tour} removeTour = {removeTour}/>
                     })
                 }
             </div>

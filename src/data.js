9d4f02062a9ca20cc92e3f8c1c0a20b43ecee31d
Md Ilham Ansari <ansariilham5@gmail.com>
2024-01-05 00:19:12 +0530

    Final commit.

--- src/data.js
+++ src/data.js
@@ -1,51 +1,59 @@
+import Agra from './assets/agra.jpeg'
+import Goa from './assets/goa.jpg'
+import Jaipur from './assets/jaipur.jpg'
+import Varanasi from './assets/varanasi.jpg'
+import Darjeeling from './assets/darjeeling.jpg'
+import Kolkata from './assets/kolkata.jpg'
+import Kochi from './assets/kochi.jpg'
+
 const data = [
     {
         id:"1",
         name: "Agra",
         info: "Agrə is a city on the banks of the Yamuna river in the Indian state of Uttar Pradesh, about 230 kilometres (140 mi) south-east of the national capital Delhi and 330 km west of the state capital Lucknow. With a population of roughly 1.6 million, Agra is the fourth-most populous city in Uttar Pradesh and twenty-third most populous city in India",
-        image: "./assets/agra.jpeg",
+        image: Agra,
         price:"35,758"
     },
     {
         id:"2",
         name: "Jaipur",
         info: "Jaipur formerly Jeypore, is the capital and the largest city of the Indian state of Rajasthan. As of 2011, the city has a population of 3.1 million, making it the tenth most populous city in the country. Located 268 km (167 miles) from the national capital New Delhi, Jaipur is also known as the Pink City due to the dominant color scheme of its buildings.",
-        image: "./assets/jaipur.jpg",
+        image: Jaipur,
         price:"82,560"
     },
     {
         id:"3",
         name: "Goa",
         info: "Goa is a state on the southwestern coast of India within the Konkan region, geographically separated from the Deccan highlands by the Western Ghats.It is bound by the Indian states of Maharashtra to the north, and Karnataka to the east and south, with the Arabian Sea in the west. It is India's smallest state by area and fourth-smallest by population. Goa has the highest GDP per capita among all Indian states,two and a half times as high as the GDP per capita of the country as a whole.The Eleventh Finance Commission of India named Goa the best-placed state because of its infrastructure, and India's National Commission on Population rated it as having the best quality of life in India.It is the second-highest ranking among  Indian states in the human development index.",
-        image: "./assets/goa.jpg",
+        image: Goa,
         price:"28,695"
     },
     {
         id:"4",
         name: "Varanasi",
         info: "Varanasi also Benares, Banaras or Kashi is a city on the Ganges river in northern India that has a central place in the traditions of pilgrimage, death, and mourning in the Hindu world.The city has a syncretic tradition of Muslim artisanship that underpins its religious tourism.Located in the middle-Ganges valley in the southeastern part of the state of Uttar Pradesh, Varanasi lies on the left bank of the river. It is 692 kilometres (430 mi) to the southeast of India's capital New Delhi and 320 kilometres (200 mi) to the southeast of the state capital, Lucknow. It lies 121 kilometres (75 mi) downstream of Prayagraj, where the confluence with the Yamuna river is another major Hindu pilgrimage site.",
-        image: "./assets/varanasi.jpg",
+        image: Varanasi,
         price:"31,095"
     },
     {
         id:"5",
         name: "Darjeeling",
         info: "Darjeeling is a town and municipality in the northernmost region of the Indian state of West Bengal. Located in the Eastern Himalayas, it has an average elevation of 2,045 metres (6,709 ft).To the west of Darjeeling lies the easternmost province of Nepal, to the east the Kingdom of Bhutan, to the north the Indian state of Sikkim, and farther north the Tibet Autonomous Region region of China. Bangladesh lies to the south and southeast, and most of the state of West Bengal lies to the south and southwest, connected to the Darjeeling region by a narrow tract. Kangchenjunga, the world's third-highest mountain, rises to the north and is prominently visible on clear days.",
-        image: "./assets/darjeeling.jpg",
+        image: Darjeeling,
         price:"78,595"
     },
     {
         id:"6",
         name: "Kolkata",
         info: "Kolkata is the capital and largest city of the Indian state of West Bengal. It lies on the eastern bank of the Hooghly River, 80 km (50 mi) west of the border with Bangladesh. It is the primary financial and commercial center of eastern and northeastern India. Kolkata is the seventh most populous city of India with an estimated city proper population of 4.5 million.It is the centre of the Kolkata Metropolitan Region, one of the most populous metropolitan areas in the world with a population of over 15 million (1.5 crore) residents. Kolkata is the de facto cultural capital of India and historically and culturally significant city in the historic region of Bengal.It is the second largest Bengali-speaking city in the world. It has the highest number of Nobel laureates among all cities in India.",
-        image: "./assets/kolkata.jpg",
+        image: Kolkata,
         price:"55,000"
     },
     {
         id:"7",
         name: "Kochi",
         info: "Kochi is a major port city along the Malabar Coast of India bordering the Laccadive Sea. It is part of the district of Ernakulam in the state of Kerala. The city is also commonly referred to as Ernakulam. As of 2011, the Kochi Municipal Corporation had a population of 677,381 within an area of 94.88 km2, and the larger Kochi urban agglomeration had a total population of more than of 2.1 million within an area of 440 km2, making it the largest and the most populous metropolitan area in Kerala. Kochi city is also part of the Greater Cochin development region and is classified as a Tier-II city by the Government of India. The civic body that governs the city is the Kochi Municipal Corporation, which was constituted in the year 1967, and the statutory bodies that oversee its development are the Greater Cochin Development Authority and the Goshree Islands Development Authority.",
-        image: "./assets/kochi.jpg",
+        image: Kochi,
         price:"10,000"
     }
 ]
